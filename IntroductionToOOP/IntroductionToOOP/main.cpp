8d89388188c54bf042f1215205783ef0181ae5c0
Clayman <Clayman@DESKTOP-DM8E77U>
2022-11-12 10:24:37 +0200

    ReadMe

---
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -0,0 +1,12 @@
+//IntroductionToOOP
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	//OOD - Object-Oriented Design (Объектно-Ориентированное Проектирование)
+	//UML - Unified Modelling Language (Унифицированный язык моделирования)
+	//OOP - Object-Oriented Programming
+
+}
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+https://github.com/okovtun/SPU_223.git
+
+
+Books:
+https://drive.google.com/drive/folders/1mIhAKMp5-Jpy6s5X8u924tRCXFEdHE_z?usp=sharing
