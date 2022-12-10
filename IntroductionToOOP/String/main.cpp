5c199f83ed1fb1a54d7c4d531cb3462ae07ff473
Clayman <Clayman@DESKTOP-DM8E77U>
2022-12-10 11:21:44 +0200

    Header initialisation DONE

--- IntroductionToOOP/String/main.cpp
+++ IntroductionToOOP/String/main.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+п»ї#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 using std::cin;
 using std::cout;
@@ -12,8 +12,8 @@ String operator+(const String& left, const String& right);
 
 class String
 {
-	size_t size;	//размер строки в Байтах (size_t - unsigned int)
-	char* str;		//адрес строки в динамической памяти
+	size_t size;	//СЂР°Р·РјРµСЂ СЃС‚СЂРѕРєРё РІ Р‘Р°Р№С‚Р°С… (size_t - unsigned int)
+	char* str;		//Р°РґСЂРµСЃ СЃС‚СЂРѕРєРё РІ РґРёРЅР°РјРёС‡РµСЃРєРѕР№ РїР°РјСЏС‚Рё
 public:
 	size_t get_size()const
 	{
@@ -29,36 +29,30 @@ public:
 	}
 
 	//				Constructors:
-	explicit String(size_t size = 80)
+	explicit String(size_t size = 80) :size(size), str(new char[size] {})
 	{
-		this->size = size;
-		this->str = new char[size] {};
 		cout.width(WIDTH);
 		cout << std::left << "DefaultConstructor:" << this << endl;
 	}
-	String(const char* str)
+	String(const char* str) :size(strlen(str) + 1), str(new char[size] {})
 	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size] {};
 		strcpy(this->str, str);	//strcpy - string copy
 		//strcpy(dst, src);
-		//dst - destination string (с трока В которую копируем - строка получатель)
-		//src - source string (строка источник - строка, из которой копируем)
+		//dst - destination string (СЃ С‚СЂРѕРєР° Р’ РєРѕС‚РѕСЂСѓСЋ РєРѕРїРёСЂСѓРµРј - СЃС‚СЂРѕРєР° РїРѕР»СѓС‡Р°С‚РµР»СЊ)
+		//src - source string (СЃС‚СЂРѕРєР° РёСЃС‚РѕС‡РЅРёРє - СЃС‚СЂРѕРєР°, РёР· РєРѕС‚РѕСЂРѕР№ РєРѕРїРёСЂСѓРµРј)
 		cout.width(WIDTH);
 		cout << std::left << "Constructor:" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other) :size(other.size), str(new char[size] {})
 	{
-		this->size = other.size;
-		this->str = new char[size] {};
+		//this->size = other.size;
+		//this->str = new char[size] {};
 		strcpy(this->str, other.str);
 		cout.width(WIDTH);
 		cout << std::left << "CopyConstructor:" << this << endl;
 	}
-	String(String&& other)
+	String(String&& other):size(other.size), str(other.str)
 	{
-		this->size = other.size;
-		this->str = other.str;
 		other.size = 0;
 		other.str = nullptr;
 		cout.width(WIDTH);
@@ -74,9 +68,9 @@ public:
 	//					Operators:
 	String& operator=(const String& other)
 	{
-		//0) Проверяем, не является ли принятый объект нашим объектом:
+		//0) РџСЂРѕРІРµСЂСЏРµРј, РЅРµ СЏРІР»СЏРµС‚СЃСЏ Р»Рё РїСЂРёРЅСЏС‚С‹Р№ РѕР±СЉРµРєС‚ РЅР°С€РёРј РѕР±СЉРµРєС‚РѕРј:
 		if (this == &other)return *this;
-		//1) Удаляем старое значение объекта
+		//1) РЈРґР°Р»СЏРµРј СЃС‚Р°СЂРѕРµ Р·РЅР°С‡РµРЅРёРµ РѕР±СЉРµРєС‚Р°
 		delete[] this->str;
 		//2) Deep copy:
 		this->size = other.size;
@@ -143,8 +137,8 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 }
 
 //#define CONSTRUCTORS_CHECK
-//#define OPERATOR_PLUS_CHECK
-#define WAYS_TO_CALL_CONSTRUCTORS
+#define OPERATOR_PLUS_CHECK
+//#define WAYS_TO_CALL_CONSTRUCTORS
 
 void main()
 {
@@ -195,12 +189,12 @@ void main()
 	String str3 = str2;		//Copy constructor
 	str3.print();
 
-	String str4();	//Здесь НЕ вызывается Default constructor, 
-					//здесь объявляется функция str4, которая ничего не принимает,
-					//и возвращает значение типа String
-	//str4.print();	//str4 - это функция, а не объект
-	//Явно вызвать Default constructor можно так:
-	String str5{};	//Явный вызов конструктора по умолчанию
+	String str4();	//Р—РґРµСЃСЊ РќР• РІС‹Р·С‹РІР°РµС‚СЃСЏ Default constructor, 
+					//Р·РґРµСЃСЊ РѕР±СЉСЏРІР»СЏРµС‚СЃСЏ С„СѓРЅРєС†РёСЏ str4, РєРѕС‚РѕСЂР°СЏ РЅРёС‡РµРіРѕ РЅРµ РїСЂРёРЅРёРјР°РµС‚,
+					//Рё РІРѕР·РІСЂР°С‰Р°РµС‚ Р·РЅР°С‡РµРЅРёРµ С‚РёРїР° String
+	//str4.print();	//str4 - СЌС‚Рѕ С„СѓРЅРєС†РёСЏ, Р° РЅРµ РѕР±СЉРµРєС‚
+	//РЇРІРЅРѕ РІС‹Р·РІР°С‚СЊ Default constructor РјРѕР¶РЅРѕ С‚Р°Рє:
+	String str5{};	//РЇРІРЅС‹Р№ РІС‹Р·РѕРІ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР° РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
 	str5.print();
 
 	String str6{ 8 };
