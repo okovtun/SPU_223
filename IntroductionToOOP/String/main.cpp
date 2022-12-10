236469841aaf2d04af9da1a599f417bf25c4b166
Clayman <Clayman@DESKTOP-DM8E77U>
2022-12-10 11:49:40 +0200

    Methods out of class

--- IntroductionToOOP/String/main.cpp
+++ IntroductionToOOP/String/main.cpp
@@ -15,102 +15,124 @@ class String
 	size_t size;	//размер строки в Байтах (size_t - unsigned int)
 	char* str;		//адрес строки в динамической памяти
 public:
-	size_t get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
+	size_t get_size()const;
+	const char* get_str()const;
+	char* get_str();
 
 	//				Constructors:
-	explicit String(size_t size = 80) :size(size), str(new char[size] {})
-	{
-		cout.width(WIDTH);
-		cout << std::left << "DefaultConstructor:" << this << endl;
-	}
-	String(const char* str) :String(strlen(str) + 1)
-	{
-		strcpy(this->str, str);	//strcpy - string copy
-		//strcpy(dst, src);
-		//dst - destination string (с трока В которую копируем - строка получатель)
-		//src - source string (строка источник - строка, из которой копируем)
-		cout.width(WIDTH);
-		cout << std::left << "Constructor:" << this << endl;
-	}
-	String(const String& other) :String(other.str)
-	{
-		cout.width(WIDTH);
-		cout << std::left << "CopyConstructor:" << this << endl;
-	}
-	String(String&& other) :size(other.size), str(other.str)
-	{
-		other.size = 0;
-		other.str = nullptr;
-		cout.width(WIDTH);
-		cout << std::left << "MoveConstructor:" << this << endl;
-	}
-	~String()
-	{
-		delete[] str;
-		cout.width(WIDTH);
-		cout << std::left << "Destructor:" << this << endl;
-	}
+	explicit String(size_t size = 80);
+	String(const char* str);
+	String(const String& other);
+	String(String&& other);
+	~String();
 
 	//					Operators:
-	String& operator=(const String& other)
-	{
-		//0) Проверяем, не является ли принятый объект нашим объектом:
-		if (this == &other)return *this;
-		//1) Удаляем старое значение объекта
-		delete[] this->str;
-		//2) Deep copy:
-		this->size = other.size;
-		this->str = new char[size] {};
-		strcpy(this->str, other.str);
-		cout.width(WIDTH);
-		cout << std::left << "CopyAssignment:" << this << endl;
-		return *this;
-	}
-	String& operator=(String&& other)
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
-		//Shallow copy:
-		this->size = other.size;
-		this->str = other.str;
-		other.size = 0;
-		other.str = nullptr;
-		cout.width(WIDTH);
-		cout << std::left << "MoveAssignment:" << this << endl;
-	}
-	String& operator+=(const String& other)
-	{
-		return *this = *this + other;
-	}
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+	String& operator+=(const String& other);
 
-	const char& operator[](int i)const
-	{
-		return str[i];
-	}
-	char& operator[](int i)
-	{
-		return str[i];
-	}
-
+	const char& operator[](int i)const;
+	char& operator[](int i);
 
 	//					Methods:
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
+	void print()const;
 };
+
+size_t String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+
+//				Constructors:
+String::String(size_t size) :size(size), str(new char[size] {})
+{
+	cout.width(WIDTH);
+	cout << std::left << "DefaultConstructor:" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)
+{
+	strcpy(this->str, str);	//strcpy - string copy
+	//strcpy(dst, src);
+	//dst - destination string (с трока В которую копируем - строка получатель)
+	//src - source string (строка источник - строка, из которой копируем)
+	cout.width(WIDTH);
+	cout << std::left << "Constructor:" << this << endl;
+}
+String::String(const String& other) :String(other.str)
+{
+	cout.width(WIDTH);
+	cout << std::left << "CopyConstructor:" << this << endl;
+}
+String::String(String&& other) :size(other.size), str(other.str)
+{
+	other.size = 0;
+	other.str = nullptr;
+	cout.width(WIDTH);
+	cout << std::left << "MoveConstructor:" << this << endl;
+}
+String::~String()
+{
+	delete[] str;
+	cout.width(WIDTH);
+	cout << std::left << "Destructor:" << this << endl;
+}
+
+//					Operators:
+String& String::operator=(const String& other)
+{
+	//0) Проверяем, не является ли принятый объект нашим объектом:
+	if (this == &other)return *this;
+	//1) Удаляем старое значение объекта
+	delete[] this->str;
+	//2) Deep copy:
+	this->size = other.size;
+	this->str = new char[size] {};
+	strcpy(this->str, other.str);
+	cout.width(WIDTH);
+	cout << std::left << "CopyAssignment:" << this << endl;
+	return *this;
+}
+String& String::operator=(String&& other)
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	//Shallow copy:
+	this->size = other.size;
+	this->str = other.str;
+	other.size = 0;
+	other.str = nullptr;
+	cout.width(WIDTH);
+	cout << std::left << "MoveAssignment:" << this << endl;
+}
+String& String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+
+//					Methods:
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
 
 String operator+(const String& left, const String& right)
 {
@@ -134,8 +156,8 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 }
 
 //#define CONSTRUCTORS_CHECK
-//#define OPERATOR_PLUS_CHECK
-#define WAYS_TO_CALL_CONSTRUCTORS
+#define OPERATOR_PLUS_CHECK
+//#define WAYS_TO_CALL_CONSTRUCTORS
 
 void main()
 {
@@ -162,8 +184,10 @@ void main()
 	String str2 = "World";
 	cout << str2 << endl;
 
+	cout << delimiter << endl;
 	str1 += str2;
 	cout << str1 << endl;
+	cout << delimiter << endl;
 
 	//cout << delimiter << endl;
 	//String str3;
